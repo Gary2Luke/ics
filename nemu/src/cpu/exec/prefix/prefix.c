3002d7317a2bd3ee746041f8f6fc548865fda142
luke <240158040@qq.com>
2017-07-12 21:12:30 +0800

    读懂了执行系统取指、译码、执行流程，宏定义好厉害！！！

--- nemu/src/cpu/exec/prefix/prefix.c
+++ nemu/src/cpu/exec/prefix/prefix.c
@@ -2,9 +2,9 @@
 
 make_helper(exec);
 
-make_helper(operand_size) {
-	ops_decoded.is_operand_size_16 = true;
+make_helper(operand_size) {						//处理0x66前缀 operand_size prefix
+	ops_decoded.is_operand_size_16 = true;		//操作数大小为16位
 	int instr_len = exec(eip + 1);
-	ops_decoded.is_operand_size_16 = false;
+	ops_decoded.is_operand_size_16 = false;			//执行完毕，恢复全局变量ops_decoded默认操作数大小为32位
 	return instr_len + 1;
 }
