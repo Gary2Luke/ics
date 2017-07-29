a349541d046f3ce8405f0d2b3393273cb58bb390
luke <240158040@qq.com>
2017-07-29 15:14:05 +0800

    修改了call指令，libc.a(消除个别浮点指令)，成功运行hello程序

--- nemu/src/cpu/exec/control/call-template.h
+++ nemu/src/cpu/exec/control/call-template.h
@@ -3,21 +3,21 @@
 #define instr call
 
 static void do_execute() {
-	//int len = get_len();
+	int len = get_len();
 	cpu.esp -= 4;		//用来push下条指令地址，指令地址为32位
 	if(op_src->type == OP_TYPE_IMM){
-		swaddr_write(cpu.esp, 4, cpu.eip + DATA_BYTE + 1, R_SS);
-		
+		swaddr_write(cpu.esp, 4, cpu.eip + len + 1, R_SS);
+		//printf("DATA_BYTE = %d, len = %d\n", DATA_BYTE, len);	
 		cpu.eip += op_src->val;	//其实这里eip应该为	cpu.eip+=1+len+op_src->val,但是cpu_exec中要加上len+1	
 		if(DATA_BYTE == 2)
 			cpu.eip &= 0x0000ffff;
-		print_asm("call $0x%x", cpu.eip+DATA_BYTE+1);
+		print_asm("call $0x%x", cpu.eip+len+1);
 
 	}
 	else{
-		swaddr_write(cpu.esp, 4, cpu.eip + 2, R_SS);
-		cpu.eip = op_src->val - 2; //其实这里eip应该为 cpu.eip==op_src->val,但是cpu_exec中要加上len+1	
-		print_asm("call $0x%x", cpu.eip + 2);
+		swaddr_write(cpu.esp, 4, cpu.eip + len + 1, R_SS);
+		cpu.eip = op_src->val - len - 1; //其实这里eip应该为 cpu.eip==op_src->val,但是cpu_exec中要加上len+1	
+		print_asm("call $0x%x", cpu.eip + len + 1);
 	
 	}
 //	print_asm("call $0x%x", cpu.eip+DATA_BYTE+1);
